7257fa031ab12f315fc38ffacb0ed857048f3ee5
YoshiBNE <yoshiyasu_express@me.com>
2022-03-19 00:33:40 +1000

    Done

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Write your code here!
 main.remove('#main');
+//or document.querySelector("main#main").remove();
 
-const newHeader = document.createElement("h1");
-newHeader.id = "victory";
+
+// const newHeader = document.createElement("h1");
+// newHeader.id = "victory";
+
+newHeader.setAttribute('id', 'victory');
 
 newHeader.innerHTML = "Yoshi is the champion";
